5115619206addda11e5f669c2eb1bad431b5669a
Soikat <193371447+soikat27@users.noreply.github.com>
2026-03-21 12:42:19 -0400

    Add initial project file structure & basic operaions
    -- Implement basic arithmetics operations for calc. app

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// Addition operation
+function add (a, b) {
+    return a+b;
+}
+
+// subtraction operation
+function subtract (a, b) {
+    return a-b;
+}
+
+// mult. operation
+function multiply (a, b) {
+    return a*b;
+}
+
+// division operation
+function divide (a, b) {
+    return a/b;
+}
